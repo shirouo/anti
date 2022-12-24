c40b56f4ff58cff7880e86af5624d96fbd5ae096
tekuuu <111714642+tekuuu@users.noreply.github.com>
2022-12-24 15:37:56 -0500

    Update and rename MOESDESYNCTEST.lua to MoesDesync.0.0.0.0.0.0.v.0.1.lua

--- MoesDesync.0.0.0.0.0.0.v.0.1.lua
+++ MoesDesync.0.0.0.0.0.0.v.0.1.lua
@@ -1,1 +1,7 @@
+--[[ made by
+
+MOE MUEHEHEHE ty for using my script
+
+]]--
+
 checkcaller=checkcaller;newcclosure=newcclosure;hookmetamethod=hookmetamethod;local v0=false;game:GetService("Players").LocalPlayer:GetMouse().KeyDown:Connect(function(v3)if (v3==string.lower(getgenv().ToggleKey)) then pcall(function()if (v0==false) then v0=true;game:GetService("StarterGui"):SetCore("SendNotification",{Title="Desync ON",Text="Thank you for using my script "   .. game:GetService("Players").LocalPlayer.DisplayName   .. "!",Icon="rbxthumb://type=AvatarHeadShot&id="   .. game:GetService("Players").LocalPlayer.UserId   .. "&w=180&h=180 true",Duration=5});elseif (v0==true) then v0=false;game:GetService("StarterGui"):SetCore("SendNotification",{Title="Desync OFF",Text="Thank you for using my script "   .. game:GetService("Players").LocalPlayer.DisplayName   .. "!",Icon="rbxthumb://type=AvatarHeadShot&id="   .. game:GetService("Players").LocalPlayer.UserId   .. "&w=180&h=180 true",Duration=5});end end);end end);local v1={};local v2=game:GetService("RunService").heartbeat:Connect(function()if (v0==true) then local v4=game.Players.LocalPlayer.Character.HumanoidRootPart.Velocity;game.Players.LocalPlayer.Character.HumanoidRootPart.v5=Vector3.new(1,1,1) * (2^16);game:GetService("RunService").RenderStepped:Wait();game.Players.LocalPlayer.Character.HumanoidRootPart.v5=v4;end end);local v1={};if (v0==true) then v2:Disconnect();end
